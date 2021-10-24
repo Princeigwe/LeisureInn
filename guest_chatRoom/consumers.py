37dd15cca6ae8ee2f0486044881a7b29ca75ff8a
Prince Igwenagha <Igwep297@gmail.com>
2021-10-24 02:43:06 +0100

    wrote consumers with channel_layer

--- guest_chatRoom/consumers.py
+++ guest_chatRoom/consumers.py
@@ -1,22 +1,72 @@
 import json
 from channels.generic.websocket import AsyncWebsocketConsumer, WebsocketConsumer
+from asgiref.sync import async_to_sync  # package for converting async methods to sync
 
 class ChatConsumer(WebsocketConsumer):
     def connect(self):
         self.room_ID = self.scope['url_route']['kwargs']['roomId'] # obtaining the 'roomId' parameter from the guestChatRoom routing of its websocket connection
         self.room_group_name = 'chatRoom_%s' % self.room_ID # creating the group name based on the roomID
         
+        # converting asynchronous channel_layer to synchronous and joining the room group...  
+        async_to_sync(self.channel_layer.group_add)(
+            self.room_group_name,
+            self.channel_name
+        )
+        
         self.accept()
     
-    def disconnect(self, close_code):
-        pass
+    def disconnect(self,close_code): # disconnect from the websocket connection
+        # leaving the room
+        async_to_sync(self.channel_layer.group_discard)(
+            self.room_group_name,
+            self.channel_name
+        )
     
-    def receive(self, text_data):
+    def receive(self, text_data): # receiving message from the websocket
         text_data_json = json.loads(text_data)
         message = text_data_json['message']
-        self.send(text_data=json.dumps({
-            'message': message
-        }))
+        
+        # sending message to room
+        async_to_sync(self.channel_layer.group_send)(
+            self.room_group_name,
+            {
+                'type': 'chat_message',
+                'message': message
+            }
+        )
+    
+    # receiving message from the group
+    def chat_message(self, event): # creating a custom event for each channel in the channel layer
+        message = event['message']
+        
+        #sending message to the Websocket
+        self.send(text_data=json.dumps(
+            {'message': message}
+        ))
+    
+    
+    
+    
+    
+    
+    
+    
+    # 2nd consumer edition
+    # def connect(self):
+    #     self.room_ID = self.scope['url_route']['kwargs']['roomId'] # obtaining the 'roomId' parameter from the guestChatRoom routing of its websocket connection
+    #     self.room_group_name = 'chatRoom_%s' % self.room_ID # creating the group name based on the roomID
+        
+    #     self.accept()
+    
+    # def disconnect(self, close_code):
+    #     pass
+    
+    # def receive(self, text_data):
+    #     text_data_json = json.loads(text_data)
+    #     message = text_data_json['message']
+    #     self.send(text_data=json.dumps({
+    #         'message': message
+    #     }))
 
     
     
@@ -42,7 +92,7 @@ class ChatConsumer(WebsocketConsumer):
     
     
     
-    
+    # 1st consumer edition
     # async def connect(self):
     #     self.room_ID = self.scope['url_route']['kwargs']['roomId'] # obtaining the 'roomId' parameter from the guestChatRoom routing of its websocket connection
     #     self.room_group_name = 'chatRoom_%s' % self.room_ID # creating the group name based on the roomID
