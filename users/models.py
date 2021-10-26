6fd9e565debb34fa176eef40142ab4cbd3c46e5e
Prince Igwenagha <Igwep297@gmail.com>
2021-10-26 19:16:19 +0100

    added profile model

--- users/models.py
+++ users/models.py
@@ -18,3 +18,14 @@ class CustomUser(AbstractUser):
     def __str__(self):
         return self.email
 
+
+class Profile(models.Model):
+    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=50, blank=True)
+    last_name = models.CharField(max_length=50, blank=True)
+    
+    class Meta:
+        verbose_name_plural = 'Profile'
+    
+    def __str__(self):
+        return (self.first_name + self.last_name)
