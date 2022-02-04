be6c60101eb13d30d963be30a9349ada2716a535
Prince Igwenagha <Igwep297@gmail.com>
2022-02-04 23:46:21 +0100

    one time payment confirmation payment

--- hotel_services/tasks.py
+++ hotel_services/tasks.py
@@ -1,5 +1,5 @@
 from celery import Celery, app #app is a decorator
-from .models import GuestCreatedSubscription
+from .models import GuestCreatedSubscription, GuestOneTimeServicePayment
 from django.core.mail import send_mail
 
 # setting up the celery app
@@ -11,3 +11,11 @@ def service_subscription_confirmation_email(guestCreatedSubscription_id):
     subject = "Subscription Payment Plan Created"
     message = f'We are notifying you that a user with this email:{guestCreatedSubscription.guest.email}, successfully created a subscription payment plan for {guestCreatedSubscription.subscription} {guestCreatedSubscription.subscription.plan} plan'
     send_mail(subject, message, "admin@leisureinn@gmail.com", [guestCreatedSubscription.guest.email], fail_silently=False)
+
+
+@app.task
+def one_time_payment_confirmation_email(id):
+    guestOneTimeServicePayment = GuestOneTimeServicePayment.objects.get(id=id)
+    subject = "Subscription Payment Plan Created"
+    message = f'We are notifying you that a user with this email:{guestOneTimeServicePayment.guest.email}, successfully paid for {guestOneTimeServicePayment.service}  for NGN{guestOneTimeServicePayment.service.price}'
+    send_mail(subject, message, "admin@leisureinn@gmail.com", [guestOneTimeServicePayment.guest.email], fail_silently=False)
