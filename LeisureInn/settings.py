3431fbdbcc8e239de5d922c659de2a35d3c57978
Prince Igwe <igwep297@gmail.com>
2022-03-08 15:16:18 +0100

    changed secret key

--- LeisureInn/settings.py
+++ LeisureInn/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'mpt=6d@pn5-cdm(j#u9cfpc%ee4%w=e(08zd-d#w$bffb_e5cf'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
