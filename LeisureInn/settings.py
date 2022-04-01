090cee9c328ad1da8f0a0944ce3a19fd3c51b864
Prince Igwe <igwep297@gmail.com>
2022-04-01 17:51:10 +0100

    email setting

--- LeisureInn/settings.py
+++ LeisureInn/settings.py
@@ -43,6 +43,13 @@ if ENVIRONMENT == 'production':
     SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https') ## to prevent redirects
     DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'  
     # EMAIL_BACKEND = "anymail.backends.sendinblue.EmailBackend"
+    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
+    EMAIL_HOST = 'smtp.gmail.com'
+    EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+    EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+    EMAIL_USE_TLS = True
+    EMAIL_PORT = 587
 
 
     # DEFAULT_FILE_STORAGE = 'storages.backends.dropbox.DropBoxStorage'
@@ -289,7 +296,8 @@ CELERY_RESULT_PERSISTENT = True
 
 # EMAIL SETTINGS
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend' # prints mail on comsole
-DEFAULT_FROM_EMAIL = 'leisureinnco@gmail.com' # the email sender email address
+# DEFAULT_FROM_EMAIL = 'leisureinnco@gmail.com' # the email sender email address
+
 
 
 # FLUTTERWAVE KEYS
