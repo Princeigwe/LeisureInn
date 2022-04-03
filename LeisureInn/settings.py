bd7438d4c3afd4c2fc0a5734558103ce54ae7582
Prince Igwe <igwep297@gmail.com>
2022-04-03 02:27:51 +0100

    env variables for email setting

--- LeisureInn/settings.py
+++ LeisureInn/settings.py
@@ -302,8 +302,8 @@ DEFAULT_FROM_EMAIL = 'leisureinnco@gmail.com' # the email sender email address
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'leisureinnco@gmail.com'
-EMAIL_HOST_PASSWORD = 'LeisureInnCo98*'
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 
